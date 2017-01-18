89aabb142b7a25c4327dbf2333f508bf629c8414
Mohamed El Zarei <mohamedelzarei@gmail.com>
2017-01-18 14:49:20 +0200

    Minor Changes.

---
+++ Full Stack Foundations/catalog/test.py
@@ -0,0 +1,12 @@
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+for x in session.query(MenuItem).all():
+    print x.name, x.restaurant.name
